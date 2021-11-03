64640494de720c05f12fc73033861ff97d6236a4
RWALKER39 <rwalker39@student.cvtc.edu>
2021-11-03 15:48:37 -0500

    typo fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnathorized: false
+        rejectUnauthorized: false
     }
 });
 
@@ -38,12 +38,12 @@ express()
     try  {
         const client = await pool.connect();
         const tables = await client.query(
-`SELECT c.relname AS table, a.attname AS column, t.typename AS type  
+`SELECT c.relname AS table, a.attname AS column, t.typname AS type  
 FROM pg_catalog.pg_class AS c 
 LEFT JOIN pg_attribute AS a
-ON c.old = a.attrelid AND a.attnum > 0
+ON c.oid = a.attrelid AND a.attnum > 0
 LEFT JOIN pg_catalog.pg_type AS t
-ON a.atttypid = t.old
+ON a.atttypid = t.oid
 WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks')
 ORDER BY c.relname, a.attnum;
     `);
@@ -77,7 +77,7 @@ ORDER BY c.relname, a.attnum;
                 VALUES (${usersId}, ${studentsId}, ${tasksId}, ${duration}) RETURNING id as new_id;`
             );
 
-            console.log(`Tracking task ${tasks_id}`);
+            console.log(`Tracking task ${tasksId}`);
 
             const result = {
                 'response' : (sqlInsert) ? (sqlInsert.rows[0]) : null
@@ -94,4 +94,4 @@ ORDER BY c.relname, a.attnum;
             res.send("Error: " + err);
         }
     })
-.listen(PORT , () => console.log('Listening on ${ PORT }'));
+.listen(PORT , () => console.log(`Listening on ${ PORT }`));
